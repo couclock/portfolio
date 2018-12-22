4859e1c30e8f4c70191f9b8bea1d1f41a4b270f0
Dany Lecoq <danylecoq@free.fr>
2018-12-22 10:10:31 +0100

    Clean useless vue

--- front/src/router.js
+++ front/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 import Stocks from '@/views/Stocks.vue';
 import Portfolios from '@/views/Portfolios.vue';
 import PortfolioDetail from '@/views/PortfolioDetail.vue';
@@ -24,14 +23,7 @@ export default new Router({
       name: 'portfolioDetail',
       component: PortfolioDetail
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
+
     { path: '*', redirect: '/portfolios' }
   ]
 });
